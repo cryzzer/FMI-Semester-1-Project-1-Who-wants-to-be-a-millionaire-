1bf07b86ba691b5d425695760a17d449a46821b6
Danail Nenkov <dpnenkov29@abv.bg>
2021-01-07 22:16:18 +0200

    Changes to menu + times question exists

--- Who-wants-to-be-a-millionaire/Who-wants-to-be-a-millionaire.cpp
+++ Who-wants-to-be-a-millionaire/Who-wants-to-be-a-millionaire.cpp
@@ -3,10 +3,27 @@
 #include <iomanip>
 
 using namespace std;
+
+char LevelFromIntToChar(int level) {//turning level number into number of ID    example: ID:01
+	return '0' + level;
+}
+void IDLookingGenerator(int level, char questionID [6]) {//creating the ID that the program will look for
+	if (level < 10) {
+		char numberOfID = LevelFromIntToChar(level);
+		questionID[3] = '0';//manual adding 0
+		questionID[4] = numberOfID;
+	}
+	else {
+		level = level % 10;
+		char numberOfID = LevelFromIntToChar(level);
+		questionID[3] = '1';//manual adding 1
+		questionID[4] = numberOfID;
+	}
+}
 void Border() {
 	cout << endl;
 	cout << setw(3);
-	for (int i = 0; i < 116;i++) {
+	for (int i = 0; i < 116;i++) {//printing border
 		cout << '=';
 	}
 	cout << endl;
@@ -15,57 +32,95 @@ void ValidInput(char& number, char string[], const int sizeOfString) {
 	bool validChoice = false;
 	do {
 		cin >> number;
-		for (int i = 0; i < sizeOfString;i++) {
-			if (number != string[i]) {
+		for (int i = 0; i < sizeOfString;i++) {//if the input does not match with any of the remaining options, continue the loop untill a viable
+			if (number != string[i]) {         // option is inputed
 				continue;
 			}
-			validChoice = true;
+			validChoice = true;//flag for stopping the function
 			break;
 		}
-	} while (!validChoice);
+	} while (!validChoice);//looping while a valid choice isn't entered
 }
 
-void StartNewGame() {
+void SearchHowManyPossibleQuestions(char nameOfFile [], int& timesQuestionExists, char questionID[]) {
+	ifstream questionFile(nameOfFile);  //opening file
+	char printingString[6] = {'\0'};    //creating an char array, that will compare later
+	char gettingLine[1000];
+	while (questionFile.getline(gettingLine, 1000)) {//getting the whole line
+		for (int i = 0; i < 6; i++) {
+			printingString[i] = gettingLine[i];//rewriting the first 5 simbols to a new char array
+		}
+		printingString[5] = '\0';//adding manual terminating zero to the last element
 
+		bool isSame = true;//creating a flag that will signal if the arrays are equal(the IDs)
+		for (int i = 0; i < 5; i++) {
+			if (printingString[i] != questionID[i]) {
+				isSame = false;
+				break;
+			}
+		}
+		if (isSame) {
+			timesQuestionExists++;//giving the integer +1 to its value
+		}
+	}
+	questionFile.close();//closing file
 }
+
+void StartNewGame(char nameOfFile []) {
+	int level = 1;
+
+	char questionID[6] = { 'I','D', ':', '\0', '\0' };
+	IDLookingGenerator(level, questionID);
+
+	bool gameLost = false;
+	//while (true) {// level > 10 || gameLost == true
+
+		int timesQuestionExists = 0;
+		SearchHowManyPossibleQuestions(nameOfFile, timesQuestionExists,questionID);
+
+
+	//}
+}
+
+
 
 void NewGame() {
+	cout << endl << endl << endl << endl << endl << endl;
 	Border();
 	cout << endl;
 	cout << setw(69) << "Choose category:\n\n";
-	cout << setw(69) << "1. Politics\n";
+	cout << setw(69) << "1. Life\n";
 	cout << setw(69) << "2. History\n";
-	cout << setw(69) << "3. Geography\n";
-	cout << setw(69) << "4. Literature\n";
-	cout << setw(69) << "5. Physics\n\n";
+	cout << setw(69) << "3. Cinema\n";
+	cout << setw(69) << "4. Sports\n";
+	cout << setw(69) << "5. Politics\n\n";
 	cout << setw(69) << "9. All categories\n\n";
+	Border();
 	cout << "Enter your choice: ";
 
 	const int sizeOfString = 10;
 	char possibleChoices[sizeOfString] = { '1', '2', '3', '4', '5', '9'};
 	char choice;
 	ValidInput(choice, possibleChoices, sizeOfString);
-	Border();//not working properly, need to show up before validation
 
-	switch (choice) {
-		case 1: {
+	if (choice == '1') {
+		char topic[20] = "Life.txt";
+		StartNewGame(topic);
+	}
+	else if (choice == '2') {
+		char topic[20] = "History.txt";
+	}
+	else if (choice == '3') {
+		char topic[20] = "Cinema.txt";
+	}
+	else if (choice == '4') {
+		char topic[20] = "Sports.txt";
+	}
+	else if (choice == '5') {
+		char topic[20] = "Politics.txt";
+	}
+	else {
 
-			break;
-		}
-		case 2: {
-			break;
-		}
-		case 3: {
-			break;
-		}
-		case 4: {
-			break;
-		}
-		case 5: {
-			break;
-		}
-		default: {
-		}
 	}
 }
 void EnteringNewQuestion() {
@@ -84,4 +139,6 @@ void MainMenu() {
 int main()
 {
 	NewGame();
+	//StartNewGame;
+	return 0;
 }
